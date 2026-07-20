f520e696e1d7228997b654970f65f0a173dafc68
Indie Seal <alxoanimations@gmail.com>
2026-07-19 21:33:21 -0700

    Upgrades instantiable from code
    Made it so instead of copying and pasting in HTML a bunch of the upgrades and having to put each unique id, I made it a centralized system in the class!
    
    I also added comas for numbers, as I don't want people counting how many digits there are.
    
    Now I gotta work on even more visuals :]

--- scripts/sealclicker.js
+++ scripts/sealclicker.js
@@ -34,6 +34,11 @@ function onSealClicked(){
     sumPoints(baseClickPoints);
 }
 
+const generalUpgradeParent = document.getElementById("general_upgrades");
+const autoclickerUpgradeParent = document.getElementById("autoclicker_upgrades");
+
+const autoclickerUpgradeRowParent = document.getElementById("upgrade-visual-rows");
+
 const scoreText = document.getElementById("sealclicker-score");
 var points = 0;
 
@@ -52,11 +57,12 @@ var allAutoclickers = [];
 var allGeneralUpgrades = [];
 
 class UpgradeInformation{
-    constructor(name, description, icon, rowIcon){
+    constructor(name, description, icon, rowImg, rowIcon){
         this.name = name;
         this.description = description;
         this.icon = icon;
 
+        this.rowImg = rowImg;
         this.rowIcon = rowIcon;
     }
 }
@@ -68,24 +74,24 @@ class Upgrade{
         this.upgradeID = upgradeID;
         this.upgradeInfo = upgradeInformation;
 
-        this.upgradeNameElement = document.getElementById(upgradeID + UPGRADE_NAME_SUFFIX);
-        this.upgradeIconElement = document.getElementById(upgradeID + UPGRADE_ICON_SUFFIX);
-
-        this.buyElement = document.getElementById(upgradeID + UPGRADE_BUY_SUFFIX);
-        this.costElement = document.getElementById(upgradeID + UPGRADE_COST_SUFFIX);
-        this.amountElement = document.getElementById(upgradeID + UPGRADE_AMOUNT_SUFFIX);
-
         this.basePrice = basePrice;
         this.priceMultiplier = priceMultiplier;
 
         this.onUpgradeBought = new CustomEvent("onUpgradeBought", {
             detail: {
-                upgrade: undefined,
+                upgrade: this,
             },
         });
     }
 
     setup = () =>{
+        this.upgradeNameElement = document.getElementById(this.upgradeID + UPGRADE_NAME_SUFFIX);
+        this.upgradeIconElement = document.getElementById(this.upgradeID + UPGRADE_ICON_SUFFIX);
+
+        this.buyElement = document.getElementById(this.upgradeID + UPGRADE_BUY_SUFFIX);
+        this.costElement = document.getElementById(this.upgradeID + UPGRADE_COST_SUFFIX);
+        this.amountElement = document.getElementById(this.upgradeID + UPGRADE_AMOUNT_SUFFIX);
+        
         this.buyElement.addEventListener("click", this.onBuyElementClicked);
         this.updateUI();
     }
@@ -96,8 +102,8 @@ class Upgrade{
     }
 
     updateUI(){
-        if(this.costElement != undefined) this.costElement.innerHTML = `${this.getPrice()}`;
-        if(this.amountElement != undefined) this.amountElement.innerHTML = `${this.amount}`;
+        if(this.costElement != undefined) this.costElement.innerHTML = `${this.getPrice().toLocaleString('en-US')}`;
+        if(this.amountElement != undefined) this.amountElement.innerHTML = `${this.amount.toLocaleString('en-US')}`;
 
         if(this.upgradeNameElement != undefined) this.upgradeNameElement.innerHTML = `${this.upgradeInfo.name}`;
         if(this.upgradeIconElement != undefined) this.upgradeIconElement.src = `${this.upgradeInfo.icon}`;
@@ -130,6 +136,34 @@ class AutoclickerUpgrade extends Upgrade{
     constructor(upgradeID, upgradeInformation, basePrice, priceMultiplier, pointsPerSecond){
         super(upgradeID, upgradeInformation, basePrice, priceMultiplier);
 
+        this.autoclickerPrefab = `
+            <button class="autoclicker_upgrade" id="${upgradeID}_buy">
+                <img class="icon" id="${upgradeID}_icon" src="./imgs/SealClicker/Click.png">
+                <div class="column" style="margin-left: 15px;">
+                    <b id="${upgradeID}_name" style="font-size: 1.5rem;">My name</b>
+                    <div class="row" style="justify-content: flex-start; width: 100%;">
+                        <img class="small_icon" src="./imgs/SealClicker/Food.png">
+                        <p class="autoclicker_cost" id="${upgradeID}_cost">0</p>
+                    </div>
+                </div>
+                <p class="quantity" id="${upgradeID}_amount">--</p>
+            </button>
+        `;
+
+        this.htmlRowPrefab = `
+            <div class="windows upgrade-rows" id="${upgradeID}_upgrade-row" style="background-image: url('./imgs/SealClicker/Seal1.jpg');">
+                <!-- Content added through 'sealclicker.js' -->
+            </div>
+        `;
+
+        autoclickerUpgradeRowParent.insertAdjacentHTML('beforeend', this.htmlRowPrefab);
+
+        this.htmlRowItemPrefab = `
+            <img src='${upgradeInformation.rowIcon}' style="width: 64px; height: 64px;">
+        `;
+
+        autoclickerUpgradeParent.insertAdjacentHTML('beforeend', this.autoclickerPrefab);
+
         this.upgradeRowElement = document.getElementById(upgradeID + UPGRADE_ROW_SUFFIX);
         this.pointsPerSecond = pointsPerSecond;
 
@@ -142,10 +176,6 @@ class AutoclickerUpgrade extends Upgrade{
                 upgrade: this,
             },
         });
-
-        this.htmlRowPrefab = `
-            <img src='${upgradeInformation.rowIcon}' style="width: 64px; height: 64px;">
-        `;
     }
 
     getProduction = function(){
@@ -167,8 +197,7 @@ class AutoclickerUpgrade extends Upgrade{
         if(!super.buyUpgrade()) return;
 
         this.upgradeRowElement.style.display = 'flex';
-        this.upgradeRowElement.insertAdjacentHTML('beforeend', this.htmlRowPrefab);
-        console.log("Spawn element");
+        this.upgradeRowElement.insertAdjacentHTML('beforeend', this.htmlRowItemPrefab);
     }
 }
 
@@ -177,6 +206,14 @@ class MultiplierUpgrade extends Upgrade{
     
     constructor(upgradeID, upgradeInformation, basePrice, onBuyFunction, conditionFunction){
         super(upgradeID, upgradeInformation, basePrice, 1);
+
+        this.upgradePrefab = `
+            <button class="general_upgrade" id="${upgradeID}_buy" style="display: none;">
+                <img src="${upgradeInformation.icon}">
+            </button>
+        `;
+
+        generalUpgradeParent.insertAdjacentHTML('beforeend', this.upgradePrefab);
 
         allGeneralUpgrades.push(this);
         
@@ -216,7 +253,7 @@ class MultiplierUpgrade extends Upgrade{
 
 function sumPoints(newPoints){
     points += newPoints;
-    scoreText.innerHTML = `Points: ${Math.floor(points)}`;
+    scoreText.innerHTML = `Points: ${Math.floor(points).toLocaleString('en-US')}`;
 }
 
 function onUpgradeBought(upgrade)
@@ -226,22 +263,41 @@ function onUpgradeBought(upgrade)
         score += element.getProduction();
     });
 
-    totalScorePerSecond.innerHTML = `per second: ${score.toFixed(1)}`;
+    totalScorePerSecond.innerHTML = `per second: ${score.toLocaleString('en-US')}`;
     document.dispatchEvent(upgrade.onUpgradeBought);
 }
 
 const totalScorePerSecond = document.getElementById("sealclicker-score_persecond");
 
-var autoclickerInfo = new UpgradeInformation("Harp Seal", "This little guy will help you collect fish from the North Atlantic!", './imgs/scorch_02.png', './imgs/AppIcons/AboutMe.png');
-var autoclickUpgrade = new AutoclickerUpgrade("autoclicker", autoclickerInfo, 10, 1.2, 0.3);
-autoclickUpgrade.setup();
+var normalSealInfo = new UpgradeInformation("Normal Seal", "This little guy will help you collect fish from the North Atlantic!", './imgs/SealClicker/NormalSeal.png', './imgs/SealClicker/Seal1.jpg', './imgs/AppIcons/AboutMe.png');
+var normalSealUpgrade = new AutoclickerUpgrade("normalseal", normalSealInfo, 10, 1.15, 0.3);
+normalSealUpgrade.setup();
 
-var generalUpgradeInfo = new UpgradeInformation("Basic Autofeeder", "This machine will feed seals automatically, making them <b>twice as efficient</b>.", './imgs/scorch_02.png', '');
+var ribbonSealInfo = new UpgradeInformation("Ribbon Seal", "It has some quite cool stripes", './imgs/SealClicker/RibbonSeal.png', './imgs/SealClicker/Seal1.jpg', './imgs/AppIcons/AboutMe.png');
+var ribbonSealUpgrade = new AutoclickerUpgrade("ribbonseal", ribbonSealInfo, 100, 1.15, 1);
+ribbonSealUpgrade.setup();
 
+var pocketSealInfo = new UpgradeInformation("Pocket Seal", "Straight out of a different universe", './imgs/SealClicker/PocketSeal.png', './imgs/SealClicker/Seal1.jpg', './imgs/AppIcons/AboutMe.png');
+var pocketSealUpgrade = new AutoclickerUpgrade("pocketseal", pocketSealInfo, 1100, 1.15, 8);
+pocketSealUpgrade.setup();
+
+var realisticSealInfo = new UpgradeInformation("Realistic Seal", "A whole new dimension", './imgs/SealClicker/RealisticSeal.png', './imgs/SealClicker/Seal1.jpg', './imgs/AppIcons/AboutMe.png');
+var realisticSealUpgrade = new AutoclickerUpgrade("realisticseal", realisticSealInfo, 12000, 1.15, 47);
+realisticSealUpgrade.setup();
+
+var outOfWorldSealInfo = new UpgradeInformation("Out of World Seal", "A whole new dimension", './imgs/SealClicker/OutOfWorldSeal.png', './imgs/SealClicker/Seal1.jpg', './imgs/AppIcons/AboutMe.png');
+var outOfWorldSealUpgrade = new AutoclickerUpgrade("outofworldseal", outOfWorldSealInfo, 130000, 1.15, 260);
+outOfWorldSealUpgrade.setup();
+
+var mysticSealInfo = new UpgradeInformation("Mystic Seal", "The god of all seals", './imgs/SealClicker/MysticSeal.png', './imgs/SealClicker/Seal1.jpg', './imgs/AppIcons/AboutMe.png');
+var mysticSealUpgrade = new AutoclickerUpgrade("mysticseal", mysticSealInfo, 1400000, 1.15, 1400);
+mysticSealUpgrade.setup();
+
+var generalUpgradeInfo = new UpgradeInformation("Basic Autofeeder", "This machine will feed seals automatically, making them <b>twice as efficient</b>.", './imgs/SealClicker/Seal1.jpg', '');
 var generalUpgrade = new MultiplierUpgrade("general", generalUpgradeInfo, 100,
     () => {
         baseClickPoints *= 2;
-        autoclickUpgrade.multipliers *= 2;
+        normalSealUpgrade.multipliers *= 2;
     }, () => points >= 10);
 generalUpgrade.setup();
 
@@ -295,7 +351,7 @@ function onTooltipEnterAutoclicker(autoclicker){
     let isAutoclicker = autoclicker instanceof AutoclickerUpgrade;
     if(!isAutoclicker) return;
     
-    tooltipExtraDescElement.innerHTML = `Production per level: ${autoclicker.pointsPerSecond.toFixed(2)}<br>Current production: ${autoclicker.getProduction().toFixed(2)}`
+    tooltipExtraDescElement.innerHTML = `Production per level: ${autoclicker.pointsPerSecond.toLocaleString('en-US')}<br>Current production: ${autoclicker.getProduction().toLocaleString('en-US')}`
 }
 
 function onTooltipEnterItem(upgrade){
@@ -304,7 +360,7 @@ function onTooltipEnterItem(upgrade){
     let upgradeInfo = upgrade.upgradeInfo;
     tooltipNameElement.innerHTML = upgradeInfo.name;
     tooltipDescriptionElement.innerHTML = upgradeInfo.description;
-    tooltipCostElement.innerHTML = upgrade.getPrice();
+    tooltipCostElement.innerHTML = upgrade.getPrice().toLocaleString('en-US');
 
     tooltipIconElement.src = upgradeInfo.icon;
 }
