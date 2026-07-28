4776bb014c27f1e1eaf8f6531766bd72a98573d5
Indie Seal <alxoanimations@gmail.com>
2026-07-28 15:31:31 -0700

    Satisfying mouse video drag
    I have succesfully made the video move, now it's time to actually show some videos on the top and bottom, which I'm not entirely sure of how I'm going to do it, but we'll see!

--- scripts/sealltok.js
+++ scripts/sealltok.js
@@ -1,3 +1,6 @@
+import { abs, clamp, lerp } from "./mathf.js";
+import { deltaTime } from "./time.js";
+
 class Video{
     constructor(url, title, description){
         this.videoURL = url;
@@ -13,38 +16,37 @@ var realSealVideo1 = new Video('https://cdn.pixabay.com/video/2024/03/22/205259-
 var realSealVideo2 = new Video('https://cdn.pixabay.com/video/2021/11/03/94465-643067851_tiny.mp4', "Aquarium seal", "Is it just me, or does it look like it's meditating? haha, it's so cuteee, SO CUTE, RAAH.");
 var realSealVideo2 = new Video('https://cdn.pixabay.com/video/2021/11/03/94464-643067850_tiny.mp4', "Resting seal", "Maybe it's dreaming about eating penguins and squids!");
 
-const videoElement = document.getElementById('sealtok-video');
-videoElement.onmousedown = startDragging;
+const videoElementTop = document.getElementById('sealtok-videoTop');
+const videoElementMiddle = document.getElementById('sealtok-videoMiddle');
+const videoElementBottom = document.getElementById('sealtok-videoBottom');
+
+videoElementTop.onmousedown = startDragging;
 
 function changeVideo(){
-    videoElement.src = realSealVideo2.videoURL;
-    videoElement.play();
+    videoElementTop.src = realSealVideo2.videoURL;
+    videoElementTop.play();
 }
 
 var initialY = 0;
 var currentY = 0;
 var offsetY = 0;
+const offsetForNew = 500;
 
 function startDragging(e){
-    console.log("Start");
-
     e = e || window.event;
     e.preventDefault();
 
-    const rect = videoElement.getBoundingClientRect();
+    const rect = videoElementTop.getBoundingClientRect();
     initialY = e.clientY;
-    console.log(`Initial: ${initialY}`);
 
-    currentY = rect.top;
-    offsetY = currentY;
+    offsetY = e.clientY - initialY;
+    currentY = offsetY;
 
     document.onmouseup = stopDragging;
     document.onmousemove = dragElement;
 }
 
 function dragElement(e){
-    console.log("Move");
-
     e = e || window.event;
     e.preventDefault();
 
@@ -54,15 +56,29 @@ function dragElement(e){
 
     let windowHeight = window.innerHeight;
     let maxY = windowHeight - rect.height;
-
-    console.log(offsetY);
-
-    videoElement.style.marginTop = `${offsetY}px`;
 }
 
 function stopDragging(){
-    console.log("Stop");
-
+    if(offsetY < -offsetForNew){
+        offsetY = -700;
+    }
+    else if(offsetY > offsetForNew){
+        offsetY = 700;
+    }
+    else offsetY = 0;
+    
     document.onmouseup = null;
     document.onmousemove = null;
+}
+
+var dragVelocity = 6;
+
+moveElementFunction();
+function moveElementFunction(){
+    requestAnimationFrame(moveElementFunction);
+
+    currentY = lerp(currentY, offsetY, dragVelocity * deltaTime);
+    console.log(`Current Y: ${currentY}`);
+
+    videoElementTop.style.marginTop = (currentY) + "px";
 }
